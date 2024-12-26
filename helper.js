83395580616793050a0378173e79228bac78cfc8
web358 <iva.a.atanasova@abv.bg>
2024-12-26 17:58:35 +0200

    Update helper.js

--- helper.js
+++ helper.js
@@ -118,3 +118,18 @@ btn.addEventListener('click', () => {
   document.querySelectorAll(".animate-text").forEach(e => {
     animateP(e)
   })
+
+  
+    const menuIcon = document.querySelector('.nav__icon-menu');
+const nav = document.querySelector('.nav');
+const navLinks = document.querySelectorAll('.nav__link');
+
+menuIcon.addEventListener('click', () => {
+  nav.classList.toggle('active');
+})
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    nav.classList.remove('active');
+  })
+})
